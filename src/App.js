a5d4dc98b75e3141ef507cd04b3232f57624b306
milou <milouw56@gmail.com>
2025-05-13 14:30:02 +0900

    [init] chakra provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ProfilePage from './pages/ProfilePage';
-import Header from './components/Header'
-import Navbar from './components/Navbar';
-import GlobalStyle from './styles/GlobalStyle';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import GlobalStyle from "./styles/GlobalStyle";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <Router>
-      <GlobalStyle />
-      <Header />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <GlobalStyle />
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 };
 
