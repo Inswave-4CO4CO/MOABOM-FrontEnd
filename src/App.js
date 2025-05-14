960c3e8ff54bf2b91adc58943ffccef5b621adb9
dorazi0423 <dorazi0423@gmail.com>
2025-05-14 14:52:31 +0900

    [style] : App.js 테스트 코드 삭제 및 이미지 삭제

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -7,8 +6,6 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { Review } from "./components/Review";
-import styled from "styled-components";
 
 const App = () => {
   return (
@@ -17,28 +14,6 @@ const App = () => {
         <GlobalStyle />
         <Header />
         <Navbar />
-        <ReviewContainer>
-          <Review
-            imagePath={"image/kimheewon.png"}
-            nickname={"도라지"}
-            date={"2022-05-11"}
-            rating={3.5}
-            text={
-              "ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㅓㅓㅓㅓㅓㅓㅓㅓ"
-            }
-            title={"조명가게"}
-          />
-          <Review
-            imagePath={"image/kimheewon.png"}
-            nickname={"도라지"}
-            date={"2022-05-11"}
-            rating={3.5}
-            text={"ㅇ"}
-            isUser={true}
-          />
-          <Review />
-          <Review />
-        </ReviewContainer>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -50,12 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-const ReviewContainer = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
-  background-color: pink;
-`;
