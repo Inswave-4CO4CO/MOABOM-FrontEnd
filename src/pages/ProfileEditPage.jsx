83c615951ea37d019ab75ba5b31eeaeded59cede
milou <milouw56@gmail.com>
2025-05-19 16:12:49 +0900

    [style] profile edit page init

--- src/pages/ProfileEditPage.jsx
+++ src/pages/ProfileEditPage.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import LabelInput from "../components/LabelInput";
+import BodyButton from "../components/BodyButton";
+
 const ProfileEditPage = () => {
-  return <div>hi</div>;
+  const [nickName, setNickName] = useState();
+
+  // 기존 정보 가져오기
+
+  //
+
+  return (
+    <form>
+      <LabelInput
+        label="닉네임"
+        value={nickName}
+        onChange={(e) => setNickName(e.target.value)}
+      />
+      <BodyButton>변경</BodyButton>
+    </form>
+  );
 };
 
 export default ProfileEditPage;
