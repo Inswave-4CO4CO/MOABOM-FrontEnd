bbdfa60d353ead98e0f37b0ad573e90e677f89a7
milou <milouw56@gmail.com>
2025-05-26 11:36:09 +0900

    [feat] 없을 경우 비어있음음

--- src/components/PosterSwiperSection.jsx
+++ src/components/PosterSwiperSection.jsx
@@ -4,6 +4,7 @@ import { Navigation } from "swiper/modules";
 
 import PosterCard from "./PosterCard";
 import { useNavigate } from "react-router-dom";
+import { DynamicMessage } from "../styles/components/ContentBox";
 
 const PosterSwiperSection = ({ title, data, isLoading }) => {
   const navigate = useNavigate();
@@ -19,33 +20,39 @@ const PosterSwiperSection = ({ title, data, isLoading }) => {
         slidesPerGroup={5}
         navigation
       >
-        {isLoading
-          ? Array.from({ length: 5 }).map((_, idx) => (
-              <SwiperSlide key={idx} style={{ width: "100%" }}>
-                <Flex justify="center" align="center" h="100%" my={4}>
-                  <Skeleton
-                    height="300px"
-                    width="250px"
-                    borderRadius="10px"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                  />
-                </Flex>
-              </SwiperSlide>
-            ))
-          : data.map((item) => (
-              <SwiperSlide key={item.contentId} style={{ width: "100%" }}>
-                <Flex justify="center" align="center" h="100%" my={4}>
-                  <PosterCard
-                    title={item.title}
-                    src={item.poster}
-                    ottname={item.ottName}
-                    onClick={() => navigate(`/detail/${item.contentId}`)}
-                  />
-                </Flex>
-              </SwiperSlide>
-            ))}
+        {isLoading ? (
+          Array.from({ length: 5 }).map((_, idx) => (
+            <SwiperSlide key={idx} style={{ width: "100%" }}>
+              <Flex justify="center" align="center" h="100%" my={4}>
+                <Skeleton
+                  height="300px"
+                  width="250px"
+                  borderRadius="10px"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                />
+              </Flex>
+            </SwiperSlide>
+          ))
+        ) : data.length === 0 ? (
+          <Flex justify="center" align="center" h="200px" my={4}>
+            <DynamicMessage>이곳은 비어있어요</DynamicMessage>
+          </Flex>
+        ) : (
+          data.map((item) => (
+            <SwiperSlide key={item.contentId} style={{ width: "100%" }}>
+              <Flex justify="center" align="center" h="100%" my={4}>
+                <PosterCard
+                  title={item.title}
+                  src={item.poster}
+                  ottname={item.ottName}
+                  onClick={() => navigate(`/detail/${item.contentId}`)}
+                />
+              </Flex>
+            </SwiperSlide>
+          ))
+        )}
       </Swiper>
     </Box>
   );
