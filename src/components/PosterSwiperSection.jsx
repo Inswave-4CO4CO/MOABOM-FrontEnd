105b34779c17a59c236b7bb577706adb9a04f2b9
milou <milouw56@gmail.com>
2025-05-26 11:27:29 +0900

    [feat] mainpage skeleton

--- src/components/PosterSwiperSection.jsx
+++ src/components/PosterSwiperSection.jsx
@@ -1,11 +1,11 @@
-import { Box, Heading, Flex } from "@chakra-ui/react";
+import { Box, Heading, Flex, Skeleton } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 
 import PosterCard from "./PosterCard";
 import { useNavigate } from "react-router-dom";
 
-const PosterSwiperSection = ({ title, data }) => {
+const PosterSwiperSection = ({ title, data, isLoading }) => {
   const navigate = useNavigate();
   return (
     <Box my={10} px={4} w="80%" mx="auto">
@@ -19,23 +19,38 @@ const PosterSwiperSection = ({ title, data }) => {
         slidesPerGroup={5}
         navigation
       >
-        {data.map((item) => (
-          <SwiperSlide key={item.contentId} style={{ width: "100%" }}>
-            <Flex
-              justify="center"
-              align="center"
-              h="100%"
-              my={4}
-              onClick={() => navigate(`/detail/${item.contentId}`)}
-            >
-              <PosterCard
-                title={item.title}
-                src={item.poster}
-                ottname={item.ottName}
-              />
-            </Flex>
-          </SwiperSlide>
-        ))}
+        {isLoading
+          ? Array.from({ length: 5 }).map((_, idx) => (
+              <SwiperSlide key={idx} style={{ width: "100%" }}>
+                <Flex justify="center" align="center" h="100%" my={4}>
+                  <Skeleton
+                    height="300px"
+                    width="250px"
+                    borderRadius="10px"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  />
+                </Flex>
+              </SwiperSlide>
+            ))
+          : data.map((item) => (
+              <SwiperSlide key={item.contentId} style={{ width: "100%" }}>
+                <Flex
+                  justify="center"
+                  align="center"
+                  h="100%"
+                  my={4}
+                  onClick={() => navigate(`/detail/${item.contentId}`)}
+                >
+                  <PosterCard
+                    title={item.title}
+                    src={item.poster}
+                    ottname={item.ottName}
+                  />
+                </Flex>
+              </SwiperSlide>
+            ))}
       </Swiper>
     </Box>
   );
